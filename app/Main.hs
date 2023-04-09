33fc849fae013c84ffc191086915333bd98af7ee
Tommy Bidne <tbidne@protonmail.com>
2023-04-09 14:38:58 +1200

    Remove unnecessary stacktraces

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,9 @@ module Main (main) where
 import GHC.Conc.Sync (setUncaughtExceptionHandler)
 import SafeRm.Prelude
   ( ExceptionCS (..),
-    catchCS,
+    catch,
     displayException,
-    throwCS,
+    throwM,
   )
 import SafeRm.Runner (runSafeRm)
 import System.Exit (ExitCode (..))
@@ -18,7 +18,10 @@ main = do
   setUncaughtExceptionHandler $ \ex -> putStrLn ("\n" <> displayException ex)
 
   runSafeRm
-    `catchCS` \case
+    `catch` \case
       -- For optparse applicative
       (MkExceptionCS ExitSuccess _) -> pure ()
-      other -> throwCS other
+      -- We throw ExitFailure for a number of errors in SafeRm. In these cases
+      -- we do not want to print the CallStack as it is just noise. We only
+      -- want callstacks for other errors i.e. unexpected ones.
+      (MkExceptionCS ex@(ExitFailure _) _) -> throwM ex
