3d3a459b3e487d5a221b2a78f2a5daeb7e604056
Tommy Bidne <tbidne@protonmail.com>
2023-05-02 16:45:12 +1200

    Remove unused instance / functions

--- src/SafeRm/Data/PathData/Core.hs
+++ src/SafeRm/Data/PathData/Core.hs
@@ -7,25 +7,18 @@
 module SafeRm.Data.PathData.Core
   ( -- * PathData
     PathData (..),
-    toPathData,
+    --toPathData,
     headerNames,
   )
 where
 
-import Data.ByteString.Char8 qualified as C8
-import Data.HashSet qualified as Set
-import Data.Text qualified as T
-import Effects.FileSystem.PathSize (PathSizeResult (..), pathSizeRecursive)
 import GHC.Exts (IsList)
 import GHC.Exts qualified as Exts
-import SafeRm.Data.PathData.Common qualified as Common
 import SafeRm.Data.PathType (PathType)
 import SafeRm.Data.Paths (PathI, PathIndex (..))
-import SafeRm.Data.Serialize (Serialize (..), decodeUnit)
 import SafeRm.Data.Timestamp (Timestamp)
 import SafeRm.Prelude
 import SafeRm.Utils qualified as U
-import Text.Read qualified as TR
 
 -- | Data for a path. Maintains an invariant that the original path is not
 -- the root nor is it empty.
@@ -61,83 +54,3 @@ instance Pretty PathData where
 -- | Header names.
 headerNames :: (IsList a, IsString (Exts.Item a)) => a
 headerNames = ["Type", "Name", "Original", "Size", "Created"]
-
--- | For a given filepath, attempts to capture the following data:
---
--- * Canonical path.
--- * Unique name to be used in the trash directory.
--- * File/directory type.
-toPathData ::
-  ( HasCallStack,
-    MonadLoggerNS m,
-    MonadPathReader m,
-    MonadPathSize m,
-    MonadTerminal m,
-    MonadThrow m
-  ) =>
-  Timestamp ->
-  PathI TrashHome ->
-  PathI TrashEntryOriginalPath ->
-  m (PathData, PathType)
-toPathData currTime trashHome origPath = addNamespace "toPathData" $ do
-  (fileName', originalPath', pathType) <- Common.getPathInfo trashHome origPath
-
-  size <-
-    fmap (MkBytes @B) $
-      pathSizeRecursive (originalPath' ^. #unPathI) >>= \case
-        PathSizeSuccess n -> pure n
-        PathSizePartial errs n -> do
-          -- We received a value but had some errors.
-          putStrLn "Encountered errors retrieving size. See logs."
-          for_ errs $ \e -> $(logError) (T.pack $ displayException e)
-          pure n
-
-  pure
-    ( UnsafePathData
-        { fileName = fileName',
-          originalPath = originalPath',
-          pathType,
-          size,
-          created = currTime
-        },
-      pathType
-    )
-
-instance Serialize PathData where
-  type DecodeExtra PathData = PathI TrashEntryFileName
-
-  encode :: PathData -> ByteString
-  encode pd =
-    C8.unlines
-      [ "[Trash Info]",
-        "Path=" <> encode (pd ^. #originalPath),
-        "DeletionDate=" <> encode (pd ^. #created),
-        "Size=" <> pd ^. (#size % _MkBytes % shown % packed % packedbs),
-        "Type=" <> encode (pd ^. #pathType)
-      ]
-
-  decode :: PathI TrashEntryFileName -> ByteString -> Either String PathData
-  decode name bs = do
-    mp <- Common.parseTrashInfoMap expectedKeys bs
-
-    originalPath <- decodeUnit =<< Common.lookup "Path" mp
-    created <- decodeUnit =<< Common.lookup "DeletionDate" mp
-    size <- decodeSize =<< Common.lookup "Size" mp
-    pathType <- decodeUnit =<< Common.lookup "Type" mp
-
-    Right $
-      UnsafePathData
-        { fileName = name,
-          originalPath,
-          pathType,
-          size,
-          created
-        }
-    where
-      expectedKeys = Set.fromList ["Path", "DeletionDate", "Size", "Type"]
-
-      decodeSize bs' = do
-        let bytesStr = C8.unpack bs'
-        case TR.readMaybe bytesStr of
-          Nothing -> Left $ "Could not read bytes: " <> bytesStr
-          Just n -> Right $ MkBytes n
