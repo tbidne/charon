28a2a9b8e1bfcdb74ca3bcbd84f16a88393ece1f
Tommy Bidne <tbidne@protonmail.com>
2023-05-04 13:53:01 +1200

    Fix benchmarks

--- benchmarks/Benchmarks/Prelude.hs
+++ benchmarks/Benchmarks/Prelude.hs
@@ -1,14 +1,9 @@
 -- | Prelude for benchmarks.
 module Benchmarks.Prelude
   ( module X,
-    header,
   )
 where
 
-import Data.List qualified as L
-import Data.Text qualified as T
-import SafeRm.Data.Backend (Backend (..))
-import SafeRm.Data.PathData (headerNames)
 import SafeRm.Prelude as X
 import Test.Tasty.Bench as X
   ( Benchmark,
@@ -18,7 +13,3 @@ import Test.Tasty.Bench as X
     nfIO,
   )
 import Test.Utils as X (clearDirectory)
-
--- | Csv header.
-header :: ByteString
-header = encodeUtf8 . T.pack . L.intercalate "," $ headerNames BackendCbor
